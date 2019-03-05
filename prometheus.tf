cac4a93ff49321e31243556260f30a6b5011a221
Bogdan <bfrunza@anvibo.com>
2019-03-05 14:13:52 +0200

    adding prometheus service

---
+++ prometheus.tf
@@ -0,0 +1,59 @@
+resource "docker_network" "proxy" {
+  name = "proxy"
+  driver = "overlay"
+}
+resource "docker_volume" "prometheus_data" {
+  name = "prometheus_data"
+}
+data "local_file" "prometheus-yml" {
+    filename = "prometheus.yml"
+}
+resource "docker_config" "prometheus-yml" {
+  name = "prometheus-yml-${replace(timestamp(),":", ".")}"
+  data = "${base64encode(data.local_file.prometheus-yml.content)}"
+
+  lifecycle {
+    ignore_changes = ["name"]
+    create_before_destroy = true
+  }
+}
+resource "docker_service" "prometheus" {
+    name = "prometheus-service"
+
+    task_spec {
+        container_spec {
+            image = "prom/prometheus"
+
+            labels {
+                traefik.frontend.rule = "Host:prometheus.mon.anvibo.com"
+                traefik.port = 9000
+                traefik.docker.network = "${docker_network.proxy.name}"
+            }
+
+            configs = [
+                {
+                    config_id   = "${docker_config.prometheus-yml.id}"
+                    config_name = "${docker_config.prometheus-yml.name}"
+                    file_name = "/etc/prometheus/prometheus.yml"
+                },
+            ]
+
+            mounts = [
+                {
+                    target      = "/var/run/docker.sock"
+                    source      = "/var/run/docker.sock"
+                    type        = "bind"
+                    read_only   = true
+                },
+                {
+                    source      = "${docker_volume.prometheus_data.name}"
+                    target      = "/prometheus"   
+                    type        = "volume"
+                    read_only   = false
+                },
+                
+            ]
+        }
+        networks     = ["${docker_network.proxy.id}"]
+    }
+}
