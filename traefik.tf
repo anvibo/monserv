d2f1de852f403f8293137c546f8dfe918dbe9763
Bogdan <bfrunza@anvibo.com>
2019-03-05 13:42:09 +0200

    adding portainer service for testing

--- traefik.tf
+++ traefik.tf
@@ -1,10 +1,7 @@
 data "local_file" "traefik-toml" {
     filename = "traefik.toml"
 }
-resource "docker_network" "proxy" {
-  name = "proxy"
-  driver = "overlay"
-}
+
 resource "docker_config" "traefik-toml" {
   name = "traefik-toml-${replace(timestamp(),":", ".")}"
   data = "${base64encode(data.local_file.traefik-toml.content)}"
@@ -13,6 +10,11 @@ resource "docker_config" "traefik-toml" {
     ignore_changes = ["name"]
     create_before_destroy = true
   }
+}
+
+resource "docker_network" "proxy" {
+  name = "proxy"
+  driver = "overlay"
 }
 resource "docker_volume" "traefik_acme" {
   name = "traefik_acme"
@@ -65,5 +67,29 @@ resource "docker_service" "traefik" {
         target_port = "443"
         published_port = "443"
       }
+    }
+}
+
+resource "docker_service" "portainer" {
+    name = "portainer-service"
+
+    task_spec {
+        container_spec {
+            image = "portainer/portainer"
+
+            labels {
+                traefik.frontend.rule = "Host:portainer.mon.anvibo.com"
+                traefik.port = 9000
+            }
+
+            mounts = [
+                {
+                    target      = "/var/run/docker.sock"
+                    source      = "/var/run/docker.sock"
+                    type        = "bind"
+                },
+            ]
+        }
+        networks     = ["${docker_network.proxy.id}"]
     }
 }
