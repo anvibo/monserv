abb70375dc51f958500e895fe138e437ac656669
Bogdan <bfrunza@anvibo.com>
2019-03-06 17:16:38 +0200

    fixing traefik network parameter list

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ resource "docker_network" "proxy" {
 }
 module "traefik" {
   source = "services/traefik"
-  networks = "${list("${docker_network.proxy.id}")}"
+  networks = ["${split(",", join(",", docker_network.proxy.id))}"]
 }
 module "grafana" {
   source = "services/grafana"
-  networks = "${list("${docker_network.proxy.id}")}"
+  networks = ["${split(",", join(",", docker_network.proxy.id))}"]
   traefik_network = "${docker_network.proxy.name}"
 }
 module "prometheus" {
   source = "services/prometheus"
-  networks = "${list("${docker_network.proxy.id}")}"
+  networks = ["${split(",", join(",", docker_network.proxy.id))}"]
   traefik_network = "${docker_network.proxy.name}"
 }
